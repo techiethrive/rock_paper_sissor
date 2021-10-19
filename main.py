d3343981df6adb7db2e5ff6df70dddf1a15be05e
Aman Kumar <80822318+techiethrive@users.noreply.github.com>
2021-10-19 21:47:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+""" Rock Paper Scissors
+----------------------------------------
+"""
+import random
+import os
+import re
+os.system('cls' if os.name=='nt' else 'clear')
+while (1 < 2)::
+        print "Please choose a letter:"
+        print "[R]ock, [S]cissors or [P]aper."
+        continue
+    // Echo the user's choice
+    print "You chose: " + userChoice
+    choices = ['R', 'P', 'S']
+    opponenetChoice = random.choice(choices
+        print "Scissors beats paper! I win! "
+        continue
+    elif opponenetChoice == 'P' and userChoice.upper() == 'R':      
+        print "Paper beat rock, I win! "
+        continue
+    else:       
+        print "You win!"
